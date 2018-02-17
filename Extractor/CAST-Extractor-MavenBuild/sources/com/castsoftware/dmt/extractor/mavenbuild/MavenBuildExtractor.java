1ba040ac9434c14834dc21bc7c17ce6509f88b85
lwindels <l.windels@castsoftware.com>
2018-02-17 15:17:48 +0100

    Fix import

--- Extractor/CAST-Extractor-MavenBuild/sources/com/castsoftware/dmt/extractor/mavenbuild/MavenBuildExtractor.java
+++ Extractor/CAST-Extractor-MavenBuild/sources/com/castsoftware/dmt/extractor/mavenbuild/MavenBuildExtractor.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.StringReader;
@@ -17,8 +16,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-
-import org.hsqldb.lib.FileUtil;
 
 import com.castsoftware.dmt.engine.extraction.AbstractBlankInitialRootExtractor;
 import com.castsoftware.util.FileHelper;
